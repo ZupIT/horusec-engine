9751118e3d55da75c979cb9d3b972db7126dc5e5
Matheus Alcantara <matheus.alcantara@zup.com.br>
2021-09-17 18:53:26 -0300

    javascript/ast: initial implementation of generic ast
    This commit introduce the first initial implementation of a generic
    AST. This initial implementation consist of creating 4 main types of
    interface. A Node that represents some Node on AST, a Decl that
    represents any Node declaration, an Expr that represents any expression
    and Stmt that represents statements. For these 4 interfaces we have some
    structs that implement these interfaces. These structs is used during
    parse some CST to a generic AST. Using these fixed types of
    interfaces/structs we can more easily implement an engine that search
    for code vulnerabilities, converting multiple ways to represents the same
    thing to one representation. E.g arrow function are converted to a simple
    function declaration.
    This commit also introduce the initial implementation of converting
    Javascript CST to a generic AST. This initial implementation can parse
    top level imports, variable declaration and class declaration with
    fields and simple methods.
    
    Co-authored-by: Matheus Alcantara <matheus.alcantara@zup.com.br>
    Signed-off-by: Matheus Alcantara <matheus.alcantara@zup.com.br>

---
+++ internal/ast/walk.go
@@ -0,0 +1,174 @@
+// Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ast
+
+import "fmt"
+
+// A Visitor's Visit method is invoked for each node encountered by Walk.
+// If the result visitor w is not nil, Walk visits each of the children
+// of node with the visitor w, followed by a call of w.Visit(nil).
+type Visitor interface {
+	Visit(node Node) (w Visitor)
+}
+
+// Inspect traverses an AST in depth-first order: It starts by calling
+// f(node); node must not be nil. If f returns true, Inspect invokes f
+// recursively for each of the non-nil children of node, followed by a
+// call of f(nil).
+func Inspect(node Node, f func(Node) bool) {
+	Walk(inspector(f), node)
+}
+
+// Walk traverses an AST in depth-first order: It starts by calling
+// v.Visit(node); node must not be nil. If the visitor w returned by
+// v.Visit(node) is not nil, Walk is invoked recursively with visitor
+// w for each of the non-nil children of node, followed by a call of
+// w.Visit(nil).
+func Walk(v Visitor, node Node) {
+	if v = v.Visit(node); v == nil {
+		return
+	}
+
+	// walk children
+	// (the order of the cases matches the order
+	// of the corresponding node types in ast.go)
+	switch n := node.(type) {
+
+	// Expressions
+	case *Ident, *BasicLit:
+		// Nothing to do.
+	case *Field:
+		if n.Name != nil {
+			Walk(v, n.Name)
+		}
+	case *FieldList:
+		for _, f := range n.List {
+			Walk(v, f)
+		}
+	case *FuncType:
+		if n.Params != nil {
+			Walk(v, n.Params)
+		}
+		if n.Results != nil {
+			Walk(v, n.Results)
+		}
+	case *FuncLit:
+		Walk(v, n.Type)
+		Walk(v, n.Body)
+	case *TemplateExpr:
+		for _, expr := range n.Subs {
+			Walk(v, expr)
+		}
+	case *ObjectExpr:
+		if n.Name != nil {
+			Walk(v, n.Name)
+		}
+		if n.Type != nil {
+			Walk(v, n.Type)
+		}
+		walkExprList(v, n.Elts)
+	case *BinaryExpr:
+		Walk(v, n.Left)
+		Walk(v, n.Right)
+	case *SelectorExpr:
+		Walk(v, n.Expr)
+		if n.Sel != nil {
+			Walk(v, n.Sel)
+		}
+	case *CallExpr:
+		Walk(v, n.Fun)
+		walkExprList(v, n.Args)
+	case *KeyValueExpr:
+		Walk(v, n.Key)
+		Walk(v, n.Value)
+
+	// Statements
+	case *AssignStmt:
+		walkExprList(v, n.Lhs)
+		walkExprList(v, n.Rhs)
+	case *BlockStmt:
+		walkStmtList(v, n.List)
+	case *ExprStmt:
+		Walk(v, n.Expr)
+	case *ReturnStmt:
+		walkExprList(v, n.Results)
+	case *IfStmt:
+		Walk(v, n.Cond)
+		Walk(v, n.Body)
+		if n.Else != nil {
+			Walk(v, n.Else)
+		}
+
+	// Declarations
+	case *ImportDecl:
+		if n.Name != nil {
+			Walk(v, n.Name)
+		}
+		if n.Path != nil {
+			Walk(v, n.Path)
+		}
+	case *ValueDecl:
+		walkIdentList(v, n.Names)
+		walkExprList(v, n.Values)
+	case *FuncDecl:
+		Walk(v, n.Name)
+		Walk(v, n.Type)
+		if n.Body != nil {
+			Walk(v, n.Body)
+		}
+
+	// Files
+	case *File:
+		if n.Name != nil {
+			Walk(v, n.Name)
+		}
+		walkDeclList(v, n.Decls)
+	default:
+		panic(fmt.Sprintf("ast.Walk: unexpected node type %T", n))
+	}
+}
+
+func walkDeclList(v Visitor, list []Decl) {
+	for _, x := range list {
+		Walk(v, x)
+	}
+}
+
+func walkIdentList(v Visitor, list []*Ident) {
+	for _, x := range list {
+		Walk(v, x)
+	}
+}
+
+func walkStmtList(v Visitor, list []Stmt) {
+	for _, x := range list {
+		Walk(v, x)
+	}
+}
+
+func walkExprList(v Visitor, list []Expr) {
+	for _, x := range list {
+		Walk(v, x)
+	}
+}
+
+type inspector func(Node) bool
+
+func (f inspector) Visit(node Node) Visitor {
+	if f(node) {
+		return f
+	}
+	return nil
+}
