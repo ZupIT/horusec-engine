217eb2edca3dd9fd13fd78a63e892c88ea10328f
Matheus Alcantara <matheus.alcantara@zup.com.br>
2022-01-13 10:50:13 -0300

    javascript:chore - use a better approach to test parsing
    Previously the tests of parsing Javascript to a generics AST was a bit
    tedious and redundant. Basically, a source code was used as input and
    the entire expected AST needed to be declared, which make the process of
    adding new test cases tedious and redundant, since this AST contains a
    lot of nested structs.
    
    This commit change the tests workflow to use a source code file as input
    and a file with the contents of the AST to use as expected. This
    expected file contains the output from `ast.Print`, and the
    `ast.Fprint` is used to compare the expected vs actual ASTs. Now, to add
    new test cases its only necessary create a new input file on
    `testdata/source` and the expected output AST on `testdata/expected`.
    
    Signed-off-by: Matheus Alcantara <matheus.alcantara@zup.com.br>

---
+++ internal/horusec-javascript/testdata/source/classes.js
@@ -0,0 +1,10 @@
+class C1 {}
+
+class C2{
+  a;
+  b = 10;
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ internal/horusec-javascript/testdata/source/var_declarations.js
@@ -0,0 +1,17 @@
+var a = "bar"
+
+const b = 123
+
+let c = {bar: 1.10, obj: {other: foo} }
+
+let d = new Object(1, "foo", bar, {testing: true})
+
+let e = ['foo', 10, foo]
+
+const f = 10 + 10 * (20 / x)
+
+function g(a, b = 1) {}
+
+const h = 1, b = 2;
+
+let i;
